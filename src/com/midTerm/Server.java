f7dbc96d0024bbb0d5ae05848212be4e4098be2f
armin <armines.bin2000@gmail.com>
2021-06-04 10:00:13 +0430

    Remove .idea directory from statgin area

--- src/com/midTerm/Server.java
+++ src/com/midTerm/Server.java
@@ -1,6 +1,9 @@
 package com.midTerm;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -12,16 +15,19 @@ public class Server {
     // count is the number of players
     // game is the system which holds some functionalities and status
     static ExecutorService executor = Executors.newCachedThreadPool();
+    static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
     static int count;
     static Game game;
     public static void main(String[] args) {
         try (ServerSocket welcomingSocket = new ServerSocket(7660)) {
-            System.out.println("Server Started.\nWaiting For Players to Join");
+            System.out.println("Server Started.\nWaiting For Players to Join the Game");
             while (count < 10) {
                 try {
                     Socket connectionSocket = welcomingSocket.accept();
+                    var clientHandler = new ClientHandler(connectionSocket);
+                    clientHandlers.add(clientHandler);
                     count++;
-                    var thread = new Thread(new ClientHandler(connectionSocket));
+                    var thread = new Thread(clientHandler);
                     executor.execute(thread);
                     System.out.printf("Client %d Accepted.%n%d more left.%n", count, 10 - count);
                 } catch (IOException e1) {
@@ -53,14 +59,26 @@ public class Server {
          */
         @Override
         public void run() {
-
-
+            try {
+                eavesdrop();
+                respondOrSendFeedback();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("");
+            }
         }
 
         /**
          * watch for clients responds and messages
          */
-        public void eavesdrop() {
+        public void eavesdrop() throws IOException {
+            InputStream inputStream = connectionSocket.getInputStream();
+            //TODO checkout the line of code below
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String lineOfInputText;
+            while ((lineOfInputText = reader.readLine()) != null) {
+
+            }
 
         }
 
@@ -90,6 +108,14 @@ public class Server {
          */
         public void removePlayer(Player player) {
 
+        }
+
+        /**
+         * this method add a player to the list of players
+         * @param player the new player which is going to be added
+         */
+        public void addPlayer(Player player) {
+            players.add(player);
         }
 
         /**
